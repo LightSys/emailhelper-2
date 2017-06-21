d2d654268d33d76a7b6df62a5bf9492b997d8ea2
nicholasweg <njw13a@acu.edu>
2017-06-21 17:17:48 -0600

    This does some work with file writing. It also establishes a IMAP connection, but I don't think it's secure yet.

--- app/src/main/java/com/example/ben/emailhelper/MainActivity.java
+++ app/src/main/java/com/example/ben/emailhelper/MainActivity.java
@@ -1,13 +1,114 @@
 package com.example.ben.emailhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Properties;
+import android.view.View;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Store;
+
+import java.io.FileNotFoundException;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.IOException;
+
+import java.net.URL;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.XMLReader;
+import org.xml.sax.InputSource;
 
 public class MainActivity extends AppCompatActivity {
+
+    String data = "This is a test string.";
+
+    //Read and write functions used from https://stackoverflow.com/questions/14376807/how-to-read-write-string-from-a-file-in-android
+    public void writeToFile(String data) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("emails.txt", MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    public String readFromFile() {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = openFileInput("config.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    /*public void getEmail(View view) {
+        Properties props = System.getProperties();
+        props.setProperty("mail.store.protocol", "imaps");
+        try {
+            Session session = Session.getDefaultInstance(props, null);
+            Store store = session.getStore("imaps");
+            store.connect("imaps.gmail.com", "parallelbranchtest@gmail.com", "goodMorning");
+            System.out.println(store);
+
+            Folder inbox = store.getFolder("Inbox");
+            inbox.open(Folder.READ_ONLY);
+            Message messages[] = inbox.getMessages();
+            for (Message message : messages) {
+                System.out.println(message.getSubject());
+            }
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            System.out.println("Messaging Exception.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Exception.");
+        }
+    }*/
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        writeToFile(data);
+        System.out.println(readFromFile());
     }
 }
