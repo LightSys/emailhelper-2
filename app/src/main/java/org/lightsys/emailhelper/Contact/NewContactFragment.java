c99995ee1198ba7b2100e3d6aee65f6f14dbc753
donaldrshade <donaldrshade@cedarville.edu>
2018-06-21 14:27:38 -0600

    Files missed in last commit

--- app/src/main/java/org/lightsys/emailhelper/Contact/NewContactFragment.java
+++ app/src/main/java/org/lightsys/emailhelper/Contact/NewContactFragment.java
@@ -1,10 +1,5 @@
 package org.lightsys.emailhelper.Contact;
 
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,27 +7,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import org.lightsys.emailhelper.CommonMethods;
-import org.lightsys.emailhelper.Contact.ContactFragment;
 import org.lightsys.emailhelper.DatabaseHelper;
-import org.lightsys.emailhelper.MainActivity;
 import org.lightsys.emailhelper.R;
-
-import java.util.Calendar;
-import java.util.Date;
 
 public class NewContactFragment extends android.app.Fragment {
 
     DatabaseHelper db;
-
     Button addContactButton;
     EditText firstNameField, lastNameField, emailField;
     View rootView;
 
-    public NewContactFragment() {
-        // Required empty public constructor
-    }
+    public NewContactFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,56 +26,51 @@ public class NewContactFragment extends android.app.Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        rootView = inflater.inflate(R.layout.fragment_new_contact, container, false);
-        addContactButton = (Button) rootView.findViewById(R.id.addContactButton);
-        firstNameField = (EditText) rootView.findViewById(R.id.firstNameField);
-        lastNameField = (EditText) rootView.findViewById(R.id.lastNameField);
-        emailField = (EditText) rootView.findViewById(R.id.emailField);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_new_contact, container, false);// Inflate the layout for this fragment
+        db = new DatabaseHelper(getActivity().getApplicationContext());
         addItems();
         return rootView;
     }
 
     public void addItems() {
+        firstNameField = rootView.findViewById(R.id.firstNameField);
+        lastNameField = rootView.findViewById(R.id.lastNameField);
+        emailField = rootView.findViewById(R.id.emailField);
+        addContactButton = rootView.findViewById(R.id.addContactButton);
         addContactButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        db = new DatabaseHelper(getActivity().getApplicationContext());
-                        String insertEmail = emailField.getText().toString();
-                        String insertFirstName = firstNameField.getText().toString();
-                        String insertLastName = lastNameField.getText().toString();
-                        //Contact contact = new Contact(insertEmail, insertFirstName, insertLastName);
-
-                        boolean isInserted = db.insertContactData(insertEmail, insertFirstName, insertLastName);
-
-                        emailField.getText().clear();                                               //Empties EditText field when it is added to the list
-                        firstNameField.getText().clear();                                           //Also make sure you don't clear it before you add the data to the DB
-                        lastNameField.getText().clear();
-
-                        Date today = Calendar.getInstance().getTime();
-                        boolean isConvo = db.insertConversationData(insertEmail, insertFirstName + " " + insertLastName, CommonMethods.getCurrentTime(), CommonMethods.getCurrentDate());
-                        if (isInserted) {
-                            Toast.makeText(getActivity().getApplicationContext(), getString(R.string.contact_added_prestring)+insertFirstName + " " + insertLastName +getString(R.string.contact_added_poststring), Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(getActivity().getApplicationContext(), getString(R.string.contact_not_added_prestring)+insertFirstName + " " + insertLastName+getString(R.string.contact_not_added_poststring), Toast.LENGTH_SHORT).show();
-                        }
-                        if(isConvo){
-                            Toast.makeText(getActivity().getApplicationContext(), getString(R.string.conversation_added_prestring)+insertFirstName + " " + insertLastName+getString(R.string.conversation_added_poststring), Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            Toast.makeText(getActivity().getApplicationContext(), getString(R.string.conversation_not_added_prestring)+insertFirstName + " " + insertLastName+getString(R.string.conversation_not_added_poststring), Toast.LENGTH_SHORT).show();
-                        }
-                        ContactFragment temp = new ContactFragment();
-                        temp.prepareContactData();
-                        NewContactActivity newContactActivity = (NewContactActivity) getActivity();
-                        Intent upIntent = new Intent(newContactActivity.getApplicationContext(),ContactActivity.class);
-                        newContactActivity.navigateUpTo(upIntent);
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Contact newContact = getContactFromFields();
+                    boolean isInserted = db.insertContactData(newContact);
+                    boolean isConvo = db.insertConversationData(newContact, CommonMethods.getCurrentTime(), CommonMethods.getCurrentDate());
+                    clearFields();
+                    if (isInserted) {
+                        Toast.makeText(getActivity().getApplicationContext(), getString(R.string.contact_added_prestring)+newContact.getName()+getString(R.string.contact_added_poststring), Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getActivity().getApplicationContext(), getString(R.string.contact_not_added_prestring)+newContact.getName()+getString(R.string.contact_not_added_poststring), Toast.LENGTH_SHORT).show();
                     }
+                    if(isConvo){
+                        Toast.makeText(getActivity().getApplicationContext(), getString(R.string.conversation_added_prestring)+newContact.getName()+getString(R.string.conversation_added_poststring), Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(getActivity().getApplicationContext(), getString(R.string.conversation_not_added_prestring)+newContact.getName()+getString(R.string.conversation_not_added_poststring), Toast.LENGTH_SHORT).show();
+                    }
+
+                    getActivity().finish();//ends the task and reverts to what was going on previously
                 }
+            }
         );
     }
 
+    private void clearFields() {
+        emailField.getText().clear();
+        firstNameField.getText().clear();
+        lastNameField.getText().clear();
+    }
+
+    public Contact getContactFromFields() {
+        return new Contact(emailField.getText().toString(),firstNameField.getText().toString(),lastNameField.getText().toString());
+    }
 }
